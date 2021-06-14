d34ab7c6a7a7be827314f2242f617175339f0b86
Julien Martin <julienmartin@codeassist.io>
2021-06-14 21:46:34 +0200

    feat: Add seeder

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,48 @@
-async function seed() {
+const faker = require('faker');
+const { PrismaClient } = require('@prisma/client');
 
-}
+const prisma = new PrismaClient();
+const generateRandom = (max, min) => Math.floor(Math.random() * (max - min) + min);
 
-module.exports = {
-  seed
-}
+const generateData = async () => {
+  const tweets = [];
+  const user = {
+    username: faker.internet.userName(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    email: faker.internet.email(),
+    avatar: 'https://thispersondoesnotexist.com/image',
+  };
+  Array.from({ length: generateRandom(10, 0) }).forEach(() => {
+    tweets.push({
+      text: faker.lorem.sentence(),
+    });
+  });
+  if (tweets.length) {
+    user.tweets = {
+      create: tweets,
+    };
+  }
+  await prisma.user.create({
+    data: {
+      ...user,
+    },
+  });
+};
+
+const main = async () => {
+  const promises = [];
+  Array.from({ length: generateRandom(10, 3) }).forEach(() => {
+    promises.push(generateData());
+  });
+  await Promise.all(promises);
+};
+
+main()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
