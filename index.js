76516a557e6bd543b543113eea44eb3b66ffe816
Julien Martin <julienmartin@codeassist.io>
2021-06-14 22:58:05 +0200

    feat: Add starter index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const getAllUsers = () => prisma.user.findMany();
+
+getAllUsers()
+  .then((users) => {
+    console.log(users);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
